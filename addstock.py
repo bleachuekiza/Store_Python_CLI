43e87062f11f1159458631c49a2a1bde994dc0e2
bleachuekiza <38793720+bleachuekiza@users.noreply.github.com>
2021-01-03 17:24:34 +0700

    Fix Add item Stock

--- addstock.py
+++ addstock.py
@@ -10,25 +10,41 @@ def stock():
         print("NAME = ", row[1])
         print("STOCK = ", row[2])
     time.sleep(2)
-    uid = int(input('ID :'))
-    ustock = int(input('Stock :'))
-    def updateVaribleIntoTable(USTOCK, UID):
-        try:
-            sqliteConnection = sqlite3.connect('database_store.db')
-            cursor = sqliteConnection.cursor()
-            print("Connected to SQLite")
+    uuid = int(input('ID :'))
+    uustock = int(input('Stock :'))
+    def search_id(uuid):
+        conn = sqlite3.connect('database_store.db')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM store WHERE ID=?", (uuid,))
 
-            sql_update_query = """Update store set STOCK = ? where ID = ?"""
-            data = (ustock, uid)
-            cursor.execute(sql_update_query, data)
-            sqliteConnection.commit()
-            print("Record Updated successfully")
-            cursor.close()
+        rows = cur.fetchall()
 
-        except sqlite3.Error as error:
-            print("Failed to update sqlite table", error)
-        finally:
-            if (sqliteConnection):
-                sqliteConnection.close()
-                print("The sqlite connection is closed")
-    updateVaribleIntoTable(ustock, uid)
+        for row in rows:
+            scid = row
+            uid = scid[0]
+            ustock = scid[4]+uustock
+            def updateVaribleIntoTable(USTOCK, UID):
+                try:
+                    sqliteConnection = sqlite3.connect('database_store.db')
+                    cursor = sqliteConnection.cursor()
+                    print("Connected to SQLite")
+
+                    sql_update_query = """Update store set STOCK = ? where ID = ?"""
+                    data = (ustock, uid)
+                    cursor.execute(sql_update_query, data)
+                    sqliteConnection.commit()
+                    print('UPDATE STOCK')
+                    print('ID :',uid)
+                    print('Stock :',ustock)
+                    print("Record Updated successfully")
+                    cursor.close()
+
+                except sqlite3.Error as error:
+                    print("Failed to update sqlite table", error)
+                finally:
+                    if (sqliteConnection):
+                        sqliteConnection.close()
+                        print("The sqlite connection is closed")
+            updateVaribleIntoTable(ustock, uid)
+    search_id(uuid)
+stock()
