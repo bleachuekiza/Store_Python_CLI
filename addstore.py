13f500038503ac833c773c39b29b543817eefdae
bleachuekiza <38793720+bleachuekiza@users.noreply.github.com>
2021-01-04 08:39:26 +0700

    Fix Duplicate Barcode and ItemName

--- addstore.py
+++ addstore.py
@@ -2,17 +2,13 @@ import sqlite3
 import time
 
 def item():
-    time.sleep(0.2)
-    barcode = int(input('Bar Code :'))
-    name = input('Name :')
-    price = int(input('Price :'))
     def insertVaribleIntoTable(BARCODE, NAME, PRICE):
         try:
             time.sleep(1)
             sqliteConnection = sqlite3.connect('database_store.db')
             cursor = sqliteConnection.cursor()
             print("Connected to SQLite")
-                    
+                        
             time.sleep(0.2)
             sqlite_insert_with_param = """INSERT INTO store
                             (BARCODE, NAME, PRICE) 
@@ -33,4 +29,26 @@ def item():
                 time.sleep(0.2)
                 sqliteConnection.close()
                 print("The SQLite connection is closed")
-    insertVaribleIntoTable(barcode, name, price)
+    def check_name_dup(name):
+        conn = sqlite3.connect('database_store.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM store WHERE NAME=?', (name,))
+
+        if c.fetchone():
+            print('Error : Barcode or Name Duplicate')
+        else:
+            insertVaribleIntoTable(barcode, name, price)
+    def check_barcode_dup(barcode):
+        conn = sqlite3.connect('database_store.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM store WHERE BARCODE=?', (barcode,))
+
+        if c.fetchone():
+            print('Error : Barcode or Name Duplicate')
+        else:
+            check_name_dup(name)
+    time.sleep(0.2)
+    barcode = int(input('Bar Code :'))
+    name = input('Name :')
+    price = int(input('Price :'))
+    check_barcode_dup(barcode)
